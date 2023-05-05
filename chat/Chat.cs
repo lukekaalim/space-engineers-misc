ea635acf92227a3ca9d5d4caceda24152cdf7807
Luke <lukekaalim@gmail.com>
2023-05-06 00:30:59 +1000

    Add complete Rial interpretation

--- chat/Chat.cs
+++ chat/Chat.cs
@@ -1,22 +1,45 @@
 ﻿using System;
+using System.Linq;
+using IngameScript;
+using static IngameScript.Program;
 
 public class Chat
 {
-  public void Init() {
-    var msg = Rial.Binary.Write(
-      new Rial.ArrayElement() { Value = new Rial.Element[] {
-        new Rial.StringElement() { Value = "Hello" },
-        new Rial.StringElement() { Value = "World" },
-      } }
-    );
+	public void Init()
+	{
+		var msg = Rial.Binary.Write(
+			Rial.Array()
+				.Entry(Rial.Object()
+					.Prop("Hello", "World"))
+				.Entry("Luke")
+				.Entry(10)
+				.Entry(10f)
+				.Entry(new Rial.Element[]
+				{
+					10f,
+					"What",
+					Rial.Object()
+						.Prop("cowsay", Rial.Object()
+							.Prop("prop", new Rial.Element[] { "AAA", "AAA" }))
+				})
+		).ToArray();
 
-    var elements = Rial.Binary.Read(new Rial.Binary.ReadingHead() { Bytes = msg, Position = 0 });
+		var elements = Rial.Binary.Read(msg);
+		var greeting = elements.Array
+			.Value[0]
+				.Object.Get("Hello")
+					.String;
+		var number = elements.Array
+			.Value[3].Float;
 
-    Console.WriteLine($"{elements}");
-  }
+		Console.WriteLine($"{elements}");
+		Console.WriteLine($"{greeting}");
+		Console.WriteLine($"{number}");
+	}
 }
 
-public class Program {
+
+public class Entry {
   static public void Main() {
     var chat = new Chat();
     chat.Init();
--- rial/Rial.cs
+++ rial/Rial.cs
@@ -1,25 +1,48 @@
 ﻿using System;
 using System.Text;
 using System.Linq;
+using System.Collections.Generic;
+using static IngameScript.Program.Rial;
+using System.Drawing;
+using System.Collections;
+using System.Runtime.CompilerServices;
 
-using Sandbox.ModAPI.Ingame;
+//using Sandbox.ModAPI.Ingame;
 
 namespace IngameScript
 {
-  partial class Program : MyGridProgram
+  public partial class Program
   {
     public static class Rial
-    {
-      public abstract class Element {}
+		{
+			public static ObjectElement Object() => new ObjectElement() { Value = new Tuple<string, Element>[0] };
+			public static ArrayElement Array() => new ArrayElement() { Value = new Element[0] };
+
+			public abstract class Element {
+
+				public static implicit operator Element(string value) => new StringElement { Value = value };
+				public static implicit operator Element(int value) => new IntElement { Value = value };
+				public static implicit operator Element(float value) => new FloatElement { Value = value };
+				public static implicit operator Element(byte value) => new ByteElement { Value = value };
+				public static implicit operator Element(Element[] value) => new ArrayElement { Value = value };
+				public ArrayElement Array { get { return this as ArrayElement; } }
+				public ObjectElement Object { get { return this as ObjectElement; } }
+				public StringElement String { get { return this as StringElement; } }
+				public IntElement Int { get { return this as IntElement; } }
+				public FloatElement Float { get { return this as FloatElement; } }
+				public ByteElement Byte { get { return this as ByteElement; } }
+			}
 
       public abstract class Element<T> : Element {
         public T Value;
         public override string ToString() { return Value.ToString(); }
-      };
+
+				public static implicit operator T(Element<T> element) => element.Value;
+			};
 
       public class StringElement  : Element<string> {
         public override string ToString() { return $"\"{Value}\""; }
-      };
+			};
       public class IntElement     : Element<int> {};
       public class FloatElement   : Element<float> {};
       public class ByteElement    : Element<byte> {};
@@ -27,8 +50,26 @@ namespace IngameScript
       public class NullElement    : Element {};
       public class ArrayElement   : Element<Element[]> {
         public override string ToString() { return $"[{string.Join(",", Value.Select(v => v.ToString()))}]"; }
-      };
-      public class ObjectElement  : Element<Tuple<string, Element>> {};
+				public ArrayElement Entry(Element value)
+				{
+					return new ArrayElement() { Value = Value.Concat(new[] { value }).ToArray() };
+				}
+			};
+      public class ObjectElement  : Element<Tuple<string, Element>[]>
+			{
+				public override string ToString() { return $"{{ {string.Join(",", Value.Select(v => $"{v.Item1}: {v.Item2}"))} }}"; }
+        public ObjectElement Prop(string name, Element value) {
+          var el = new Tuple<string, Element>(name, value);
+          return new ObjectElement() { Value = Value.Concat(new[] { el }).ToArray() };
+				}
+				public Element Get(string name)
+				{
+					var entry = Value.First(e => e.Item1 == name);
+					return entry.Item2;
+				}
+			};
+
+      public class ByteArray      : Element<byte[]> { }
 
 
       public static class Binary {
@@ -41,16 +82,15 @@ namespace IngameScript
           Null = 4,
           Array = 5,
           Object = 6,
-        }
+					ByteArray = 7,
+				}
 
-        public class ReadingHead {
+        public class Decoder {
           public int Position = 0;
           public byte[] Bytes;
 
-          public Tag ReadTag() {
-            return (Tag)Bytes[Position++];
-          }
-          
+					public static implicit operator Decoder(byte[] bytes) => new Decoder { Bytes = bytes };
+
           public byte[] ReadBytes(int count) {
             var slice = new byte[count];
             for (int i = 0; i < count; i++)
@@ -63,59 +103,122 @@ namespace IngameScript
             var value = BitConverter.ToInt32(Bytes, Position);
             Position += 4;
             return value;
-          }
-        }
+					}
+					public float ReadFloat()
+					{
+						var value = BitConverter.ToSingle(Bytes, Position);
+						Position += 4;
+						return value;
+					}
+				}
+        public class Encoder
+				{
+          public static byte[] Tag(Tag tag)
+          {
+            return new byte[] { (byte)tag };
+					}
+					public static IEnumerable<byte> String(string value)
+					{
+						var length = utf8.GetByteCount(value);
+						var bytes = new byte[length];
+						utf8.GetBytes(value, 0, value.Length, bytes, 0);
+            return BitConverter.GetBytes(length)
+              .Concat(bytes);
+					}
+				}
         readonly static UTF8Encoding utf8 = new UTF8Encoding();
 
-        public static Element Read(ReadingHead head) {
-          var tag = head.ReadTag();
+        public static Element Read(Decoder head) {
+					var tag = (Tag)head.ReadBytes(1)[0];
 
-          switch (tag) {
+					switch (tag) {
             case Tag.Null:
               return new NullElement();
             case Tag.Byte:
               return new ByteElement() { Value = head.ReadBytes(1)[0] };
-            case Tag.Array:
-              var elementCount = head.ReadInt();
-              var elements = new Element[elementCount];
-              for (int i = 0; i < elementCount; i++) {
-                elements[i] = Read(head);
-              }
-              return new ArrayElement() { Value = elements };
-            case Tag.String:
-              var stringLength = head.ReadInt();
-              var stringContent = utf8.GetString(head.ReadBytes(stringLength));
-              return new StringElement() { Value = stringContent };
+            case Tag.Int:
+              return new IntElement() { Value = head.ReadInt() };
+						case Tag.Float:
+							return new FloatElement() { Value = head.ReadFloat() };
+						case Tag.String:
+							{
+								var stringLength = head.ReadInt();
+								var stringContent = utf8.GetString(head.ReadBytes(stringLength));
+								return new StringElement() { Value = stringContent };
+							}
+						case Tag.Array:
+							{
+								var elementCount = head.ReadInt();
+								var elements = new Element[elementCount];
+								for (int i = 0; i < elementCount; i++)
+								{
+									elements[i] = Read(head);
+								}
+								return new ArrayElement() { Value = elements };
+							}
+            case Tag.Object:
+							{
+								var propertyCount = head.ReadInt();
+								var properties = new Tuple<string, Element>[propertyCount];
+								for (int i = 0; i < propertyCount; i++)
+								{
+									var nameLength = head.ReadInt();
+									var name = utf8.GetString(head.ReadBytes(nameLength));
+                  var value = Read(head);
+                  properties[i] = new Tuple<string, Element>(name, value);
+								}
+                return new ObjectElement() { Value = properties };
+							}
+            case Tag.ByteArray:
+							{
+								var length = head.ReadInt();
+								return new ByteArray() { Value = head.ReadBytes(length) };
+							}
             default:
               throw new NotImplementedException("Ooops");
           }
         }
 
-        public static byte[] Write(Element element) {
-          if (element is StringElement stringElement)
-          {
-            var length = utf8.GetByteCount(stringElement.Value);
-            var bytes = new byte[length + 1 + 4];
-            bytes[0] = (byte)Tag.String;
-            var lengthBytes = BitConverter.GetBytes(length);
-            Array.Copy(lengthBytes, 0, bytes, 1, 4);
-            utf8.GetBytes(stringElement.Value, 0, stringElement.Value.Length, bytes, 5);
-            return bytes;
-          }
-          if (element is ArrayElement arrayElement)
-          {
-            var lengthBytes = BitConverter.GetBytes(arrayElement.Value.Length);
-            var elementBytes = arrayElement.Value
-              .SelectMany(v => Write(v))
-              .ToArray();
+        public static IEnumerable<byte> Write(Element element)
+				{
+					if (element is NullElement)
+            return Encoder.Tag(Tag.Null);
 
-            var bytes = new byte[1 + 4 + elementBytes.Length];
-            bytes[0] = (byte)Tag.Array;
-            Array.Copy(lengthBytes, 0, bytes, 1, 4);
-            Array.Copy(elementBytes, 0, bytes, 5, elementBytes.Length);
-            return bytes;
-          }
-            throw new NotImplementedException("Ooops");
+					if (element is StringElement stringElement)
+						return Encoder.Tag(Tag.String)
+							.Concat(Encoder.String(stringElement.Value));
+
+          if (element is FloatElement floatElement)
+            return Encoder.Tag(Tag.Float)
+							.Concat(BitConverter.GetBytes(floatElement.Value));
+
+					if (element is IntElement intElement)
+						return Encoder.Tag(Tag.Int)
+							.Concat(BitConverter.GetBytes(intElement.Value));
+
+					if (element is ByteElement byteElement)
+						return Encoder.Tag(Tag.Byte)
+							.Concat(BitConverter.GetBytes(byteElement.Value));
+
+					if (element is ObjectElement objectElement)
+            return Encoder.Tag(Tag.Object)
+               .Concat(BitConverter.GetBytes(objectElement.Value.Length))
+               .Concat(objectElement.Value.SelectMany(property =>
+               {
+                 return Encoder.String(property.Item1)
+                  .Concat(Write(property.Item2));
+               }));
+
+          if (element is ArrayElement arrayElement)
+						return Encoder.Tag(Tag.Array)
+							 .Concat(BitConverter.GetBytes(arrayElement.Value.Length))
+							 .Concat(arrayElement.Value.SelectMany(entry => Write(entry)));
+					if (element is ByteArray byteArrayElement)
+						return Encoder.Tag(Tag.ByteArray)
+							 .Concat(BitConverter.GetBytes(byteArrayElement.Value.Length))
+							 .Concat(byteArrayElement.Value);
+
+					throw new NotImplementedException("Ooops");
         }
       }
     }
